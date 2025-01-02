378be6493b2becc6bea51a3ce5eba899759c4b18
boladss <anton.boladss@gmail.com>
2025-01-02 17:46:32 +0800

    feat: initial authentication using client credentials flow

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_API_TOKEN_URL } from "$env/static/private";
+
+export async function load() {
+  const clientId: string = SPOTIFY_CLIENT_ID;
+  const clientSecret: string = SPOTIFY_CLIENT_SECRET;
+  const url: string = SPOTIFY_API_TOKEN_URL;
+
+  const tokenResponse = await fetch(SPOTIFY_API_TOKEN_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/x-www-form-urlencoded",
+      'Authorization': `Basic ${btoa(`${clientId}:${clientSecret}`)}` // base64 encoded
+    },
+    body: 'grant_type=client_credentials'
+  });
+
+  const data = await tokenResponse.json();
+  // return data;
+  const token: string = data.access_token;
+}
